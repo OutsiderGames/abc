a7ed177a483ab2a22245d002fcc64461205a46e3
ddkkinf <ddkkinf@naver.com>
2015-12-29 01:21:26 +0900

    [NO-BTS] Initial Import. game scene 작성

---
+++ Assets/Scripts/Cannon.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cannon : MonoBehaviour {
+	public GameObject ballPrefab;
+	public int count;
+
+	// Use this for initialization
+	void Start () {
+		count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0)) {
+			GameObject ball = MonoBehaviour.Instantiate(ballPrefab) as GameObject;
+			ball.name = "ball" + count++;
+			Vector3 position = this.transform.position;
+			position.x += count * 0.1F;
+			ball.transform.position = position;
+		}
+	}
+}
