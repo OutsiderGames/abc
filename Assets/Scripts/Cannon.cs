f3245fbcf65f2bcb6f84876a7b8729a034988a53
Argent <ddkkinf@naver.com>
2016-01-11 02:40:43 +0900

    cannon move / fire logic 분리 작성

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -5,6 +5,8 @@ public class Cannon : MonoBehaviour {
 	private AbcConfig config;
 	private int bullet;
 	private int ballBucketSize;
+	private float moveThresholdX;
+	private float scaleY;
 
 	// Use this for initialization
 	void Start () {
@@ -14,13 +16,75 @@ public class Cannon : MonoBehaviour {
 		AbcConfig config = GameObject.Find (Menu.scene).GetComponent<AbcConfig>();
 		bullet = config.bullet;
 		ballBucketSize = config.ballBucketSize;
+		moveThresholdX = Screen.width * 0.3f;
+		scaleY = Screen.height / 20.0f;
+		Debug.Log (Screen.height);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonUp(0) && bullet > 0) {
+		if (isFire() &&
+			bullet > 0) {
 			ThrowBall ();
 		}
+		Vector2? movePosition = getMovePosition ();
+		if (movePosition.HasValue) {
+			moveTo (movePosition.Value);
+		}
+	}
+
+	Vector2? getMovePosition() {
+		if (Input.GetMouseButton (0) &&
+			isMovePosition(Input.mousePosition)) {
+			return Input.mousePosition;
+		}
+		if (Input.touches.Length > 0) {
+			foreach(Touch touch in Input.touches) {
+				if (touch.phase == TouchPhase.Began &&
+					isMovePosition(touch.position)) {
+					return touch.position;
+				}
+			}
+		}
+		return null;
+	}
+
+	bool isMovePosition(Vector2 position) {
+		return position.x < moveThresholdX;
+	}
+	bool isFirePosition(Vector2 position) {
+		return isMovePosition (position) == false;
+	}
+
+	bool isFire() {
+		if (Input.GetMouseButtonUp (0) &&
+			isFirePosition(Input.mousePosition)) {
+			return true;
+		}
+		if (Input.touches.Length > 0) {
+			foreach(Touch touch in Input.touches) {
+				if (touch.phase == TouchPhase.Ended &&
+					isFirePosition(touch.position)) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	void moveTo(Vector2 position) {
+		float start = this.transform.position.y;
+		float end = position.y / scaleY - 10;
+		float y = this.transform.position.y;
+		Debug.Log (y);
+		Debug.Log (position);
+		if (start > end) {
+			y = start - 1.0f > end ? start - 1.0f : end;
+		}
+		if (start < end) {
+			y = start + 1.0f < end ? start + 1.0f : end;
+		}
+		this.transform.position = new Vector2(this.transform.position.x, y);
 	}
 
 	void ThrowBall() {
